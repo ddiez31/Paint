479d9fd3834215d1deb8dd03788c54710dbe3f35
David <david.diez31@free.fr>
2017-01-10 16:58:50 +0100

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,59 @@
+$(document).ready(function() {
+    $(':button').click(function() {
+        var btnColor = $(this).val();
+        console.log(btnColor);
+    });
+});
+
 window.onload = function initCanvas() {
-    var paint = $(this).attr("paint");
-    var context = paint.getContext("2d");
+
+    var mouseOn = false;
+    var lastX, lastY;
+    ctx = $(this).attr("myCanvas").getContext("2d");
+
+    $('#myCanvas').mousedown(function(e) {
+        mouseOn = true;
+        dessin(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
+
+    $('#myCanvas').mousemove(function(e) {
+        if (mouseOn) {
+            dessin(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#myCanvas').mouseup(function(e) {
+        mouseOn = false;
+    });
+    $('#myCanvas').mouseleave(function(e) {
+        mouseOn = false;
+    });
+};
 
 
 
+function dessin(x, y, isDown) {
+    if (isDown) {
+        ctx.beginPath();
+        ctx.strokeStyle = btnColor;
+        ctx.lineWidth = $('#selWidth').val();
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
+        ctx.closePath();
+        ctx.stroke();
+    }
+    lastX = x;
+    lastY = y;
 
 
 
+}
+
+
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 };
